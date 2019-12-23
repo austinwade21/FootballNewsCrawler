688f047a9809d6eddad64765546774b6aefdb215
Nikolai <kolyanbuhoytip@gmail.com>
2019-12-23 22:17:55 +0800

    second milestone

--- templates/fields/facebook_message.php
+++
@@ -1,4 +0,0 @@
-<?php wp_nonce_field('post-on-social-medias-message-nonce', 'post-on-social-medias-message-nonce'); ?>
-<p>
-    <textarea name="post-on-social-medias-message" id="post-on-social-medias-message" style="width: 100%;min-height: 200px;"><?php echo $current_message; ?></textarea>
-</p
